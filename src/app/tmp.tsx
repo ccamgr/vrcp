5810a1d27c474bd7fcfc45de85ece3d05c711a26
pve <150896306+kitRinor@users.noreply.github.com>
2025-10-31 00:03:20 -0400

    fix

--- src/app/tmp.tsx
+++ src/app/tmp.tsx
@@ -1,17 +1,5 @@
 import GenericScreen from "@/components/layout/GenericScreen";
-import CardViewInstance from "@/components/view/item-CardView/CardViewInstance";
-import ListViewPipelineMessage from "@/components/view/item-ListView/ListViewPipelineMessage";
-import LoadingIndicator from "@/components/view/LoadingIndicator";
-import { spacing } from "@/configs/styles";
-import { useData } from "@/contexts/DataContext";
-import { useVRChat } from "@/contexts/VRChatContext";
-import SeeMoreContainer from "@/components/features/home/SeeMoreContainer";
-import { calcFriendsLocations } from "@/libs/funcs/calcFriendLocations";
-import { routeToFeeds, routeToFriendLocations, routeToInstance, routeToWorld } from "@/libs/route";
-import { InstanceLike } from "@/libs/vrchat";
-import { PipelineMessage } from "@/vrchat/pipline/type";
 import { useTheme } from "@react-navigation/native";
-import { useEffect, useMemo, useRef, useState } from "react";
 import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 
 
